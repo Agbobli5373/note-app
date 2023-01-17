602c537afc82d02fbcf649ba92c20a61b6cf4de2
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-01-17 04:57:15 +0000

    Updates

---
+++ precache-manifest.6e30bc3debfc3ae232d8f6b21ab603e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85c229fa7a7e686fa869",
+    "url": "/note-app/static/css/main.b0b70db3.chunk.css"
+  },
+  {
+    "revision": "85c229fa7a7e686fa869",
+    "url": "/note-app/static/js/main.85c229fa.chunk.js"
+  },
+  {
+    "revision": "b8e84d405fc7a75475f1",
+    "url": "/note-app/static/js/1.b8e84d40.chunk.js"
+  },
+  {
+    "revision": "7ea2b3f95ae2d6ecd3a6",
+    "url": "/note-app/static/js/runtime~main.7ea2b3f9.js"
+  },
+  {
+    "revision": "a5092c6c3220b29fc3cedb68f9871704",
+    "url": "/note-app/index.html"
+  }
+];
